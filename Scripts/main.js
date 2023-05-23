c6ad0bd7f59716c3c7be534f4f30ca05f687dceb
Maheryfa <maheryfa@gmail.com>
2023-05-23 21:46:23 +0300

    Première version de Black Panther

---
+++ Scripts/main.js
@@ -0,0 +1,49 @@
+let bt_menu= document.querySelector("nav #ouvrt");
+
+
+let menu= document.querySelector(".menu");
+
+
+let bt_close= document.querySelector("nav #ferm");
+
+let width = window.innerWidth;
+    console.log(width);
+
+let ouvert= ()=>{
+    menu.style.display = "block";
+    bt_menu.style.display = "none";
+     bt_close.style.display="block";
+}
+
+
+let fermer= ()=>{
+    menu.style.display = "none";
+    
+    bt_menu.style.display = "block";
+   
+    bt_close.style.display="none";
+   
+} 
+
+function handleResize() {
+    var largeurEcran = window.innerWidth;
+    // Remplacez 'monElement' par l'ID de votre élément
+  
+    if (largeurEcran > 1000) {
+      // Modifier le style de l'élément lorsque la largeur de l'écran est supérieure à 1000 pixels
+      bt_menu.style.display = "none";
+    } else {
+      // Rétablir le style par défaut lorsque la largeur de l'écran est inférieure ou égale à 1000 pixels
+      bt_menu.style.display = "block";
+    }
+  }
+  
+  // Ajouter un écouteur d'événement pour l'événement resize
+  window.addEventListener('resize', handleResize);
+  
+  // Exécution initiale pour appliquer le style en fonction de la largeur de l'écran au chargement de la page
+  handleResize();
+
+
+
+
